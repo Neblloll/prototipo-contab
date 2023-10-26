9c73fa6bc7b62ebf89573cb1f88082dc6b260115
Matheus Barros <matheusbarros1911@gmail.com>
2023-10-26 20:29:28 -0300

    Criado a tela de Cadastro de Cliente

---
+++ src/components/FormRegisterClient.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from "react-native";
+
+export function FormRegisterClient({ titleInput, type }) {
+
+    return(
+        <View style = {styles.containerInput}>
+            <Text style = {styles.titleInput}>{titleInput}</Text>
+            <TextInput
+                style = {styles.contentInput}
+                keyboardType = {type || 'default'}
+                autoCapitalize = "words"
+            />
+        </View>
+    );
+};
+
+export const ButtonForm = () => {
+
+    return(
+        <View>
+            <TouchableOpacity style = {styles.buttonFormContainer}>
+                <Text style = {{color: '#FFF', fontSize: 20}}>ENVIAR</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    containerInput: {
+        minWidth: '100%',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+        marginBottom: 15,
+    },
+
+    titleInput: {
+        fontSize: 15,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+
+    contentInput: {
+        height: 40,
+        width: '100%',
+        fontSize: 17,
+        marginTop: 15,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#000'
+    },
+
+    buttonFormContainer: {
+        height: 65,
+        minWidth: '65%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        marginVertical: 40,
+        borderRadius: 20,
+        backgroundColor: '#075A90',
+    },
+})
--- src/pages/register-client/RegisterClient.js
+++ src/pages/register-client/RegisterClient.js
@@ -1,18 +1,100 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet,Text, ScrollView } from "react-native";
+
+import {FormRegisterClient, ButtonForm} from "../../components/FormRegisterClient";
 
 export default function RegisterClient() {
 
     return(
 
-        <View/>
+        <View style = {styles.container}>
+
+            <View style = {styles.titleContainer}>
+                <View style = {styles.titleTracer} />
+                <Text style = {styles.titleText}>CADASTRAR CLIENTE</Text>
+                <View style = {styles.titleTracer} />
+            </View>
+
+            <ScrollView
+                showsVerticalScrollIndicator = {false}
+                alignItems = 'center'
+            >
+
+                <View style = {{flexDirection: 'column', width: '90%'}}>
+
+                    <FormRegisterClient titleInput = {'Nome/Razão Social'} />
+                    <FormRegisterClient titleInput = {'CPF/CNPJ'} type = {'numeric'} />
+                    <FormRegisterClient titleInput = {'Inscrição Municipal'} />
+                    <FormRegisterClient titleInput = {'Endereço'} />
+
+                    <View style = {{flexDirection: "row", justifyContent: 'space-between'}}>
+
+                        <View style = {{width: '35%'}}>
+                            <FormRegisterClient titleInput = {'Número'} type = {'numeric'} />
+                        </View>
+
+                        <View style = {{width: '60%'}}>
+                            <FormRegisterClient titleInput = {'Complemento'} />
+                        </View>
+
+                    </View>
+
+                    <View style = {{flexDirection: "row", justifyContent: 'space-between'}}>
+
+                        <View style = {{width: '60%'}}>
+                            <FormRegisterClient titleInput = {'CEP'} type = {'numeric'} />
+                        </View>
+
+                        <View style = {{width: '35%'}}>
+                            <FormRegisterClient titleInput = {'UF'} />
+                        </View>
+
+                    </View>                    
+
+                    <FormRegisterClient titleInput = {'Bairro'} />
+                    <FormRegisterClient titleInput = {'Telefone'} type = {'numeric'} />
+                    <FormRegisterClient titleInput = {'Email'} type = {'email-adress'} />
+                    <FormRegisterClient titleInput = {'Razão Reduzida'} />
+                    <FormRegisterClient titleInput = {'Data de Cadastro'} type = {'numeric'} />
+                    <FormRegisterClient titleInput = {'Indicação'} />
+                    <FormRegisterClient titleInput = {'Comissão'} type = {'numeric'} />
+
+                    <ButtonForm />
+
+                </View>
+
+            </ScrollView>
+
+        </View>
 
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#FFF',
     },
 
+    titleContainer: {
+        height: 55,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    titleTracer: {
+        height: 5,
+        width: '20%',
+        marginHorizontal: 10,
+        borderRadius: 10,
+        backgroundColor: '#000',
+    },
+
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000',
+    },
 })
